7e9870e03db2a3d0001e964363e8b683263cd6ac
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-01-18 18:05:25 -0300

    incremente la versión

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='login-api',
-          version='0.1.1a1',
+          version='0.1.1a2',
           description='Proyecto que implementa la api de login',
           url='https://github.com/pablodanielrey/login-api',
           author='Desarrollo DiTeSi, FCE',
